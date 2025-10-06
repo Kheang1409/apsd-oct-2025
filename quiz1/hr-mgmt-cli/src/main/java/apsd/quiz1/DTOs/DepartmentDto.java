2410cd32bc3cc05f19158eba28c011720c21662b
Kheang1409 <40851151+Kheang1409@users.noreply.github.com>
2025-10-06 14:36:42 -0500

    switch to record

--- quiz1/hr-mgmt-cli/src/main/java/apsd/quiz1/DTOs/DepartmentDto.java
+++ quiz1/hr-mgmt-cli/src/main/java/apsd/quiz1/DTOs/DepartmentDto.java
@@ -4,25 +4,20 @@ import java.util.List;
 
 import apsd.quiz1.Domain.Department;
 
-public class DepartmentDto {
-    public String departmentNo;
-    public String name;
-    public EmployeeDto headOfEmployee;
-    public double totalSalary;
-    public List<EmployeeDto> employees;
-
+public record DepartmentDto (
+    String departmentNo,
+    String name,
+    EmployeeDto headOfEmployee,
+    double totalSalary,
+    List<EmployeeDto> employees
+ ){
     public DepartmentDto(Department department) {
-        if (department == null) return;
-        this.departmentNo = department.getDepartmentNo().toString();
-        this.name = department.getName();
-        this.totalSalary = department.getTotalSalary();
-        if (department.getHeadOfEmployee() != null) {
-            this.headOfEmployee = new EmployeeDto(department.getHeadOfEmployee());
-        }
-        if (department.getEmployees() != null) {
-            this.employees = department.getEmployees().stream()
-                .map(EmployeeDto::new)
-                .toList();
-        }
+        this(
+            department.getDepartmentNo().toString(),
+            department.getName(),
+            department.getHeadOfEmployee() != null ? new EmployeeDto(department.getHeadOfEmployee()) : null,
+            department.getTotalSalary(),
+            department.getEmployees() != null ? department.getEmployees().stream().map(EmployeeDto::new).toList() : List.of()
+        );
     }
 }
--- quiz1/hr-mgmt-cli/src/main/java/apsd/quiz1/DTOs/EmployeeDto.java
+++ quiz1/hr-mgmt-cli/src/main/java/apsd/quiz1/DTOs/EmployeeDto.java
@@ -2,19 +2,20 @@ package apsd.quiz1.DTOs;
 
 import apsd.quiz1.Domain.Employee;
 
-public class EmployeeDto {
-    public String employeeNo;
-    public String firstName;
-    public String lastName;
-    public int yearsOfEmployment;
-    public double biweeklySalary;
-
+public record EmployeeDto(
+    String employeeNo,
+    String firstName,
+    String lastName,
+    int yearsOfEmployment,
+    double biweeklySalary
+) {
     public EmployeeDto(Employee employee) {
-        if (employee == null) return;
-        this.employeeNo = employee.getEmployeeNo().toString();
-        this.firstName = employee.getFirstName();
-        this.lastName = employee.getLastName();
-        this.yearsOfEmployment = employee.getYearsOfEmployment();
-        this.biweeklySalary = employee.getBiweeklySalary();
+        this(
+            employee.getEmployeeNo().toString(),
+            employee.getFirstName(),
+            employee.getLastName(),
+            employee.getYearsOfEmployment(),
+            employee.getBiweeklySalary()
+        );
     }
 }
